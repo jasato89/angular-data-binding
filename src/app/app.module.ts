3eb139f286f72b113f277f1073f1412b3f0cf62a
jasato89 <jasato89@gmail.com>
2022-06-21 11:50:03 +0200

    added ngStyle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
 import { BodyComponent } from './body/body.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [	
@@ -18,7 +19,8 @@ import { BodyComponent } from './body/body.component';
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,11 +11,23 @@ export class HeaderComponent implements OnInit {
   date: string;
   placeholder: string;
   isDisabled: boolean;
+  textArea: string;
+  elements: Array<string>;
+  backgroundColor: string;
+  color: string;
+  isDarkModeEnabled: boolean;
+  darkText: string;
 
   constructor() {
     this.date = new Date().toLocaleDateString("es") + " " + new Date().toLocaleTimeString("es");
     this.placeholder = "Inserta tu texto aqui";
     this.isDisabled = true;
+    this.textArea = '';
+    this.elements = [];
+    this.backgroundColor = "white";
+    this.color = "black";
+    this.isDarkModeEnabled = false;
+    this.darkText = "Activar modo oscuro";
   }
 
   ngOnInit() {
@@ -27,9 +39,32 @@ export class HeaderComponent implements OnInit {
     this.isDisabled = !this.isDisabled;
   }
 
-  showAlert(){
-    const input = document.getElementById('input') as HTMLInputElement | null;
-    window.alert(input?.value);
+  showAlert() {
+    window.alert(this.textArea);
+
+  }
+
+  addElement() {
+    this.elements.push(this.textArea);
+
+  }
+
+  enableDarkMode() {
+    if (this.isDarkModeEnabled) {
+      this.backgroundColor = "white";
+      this.color = "black";
+      this.darkText = "Activar modo oscuro";
+      this.isDarkModeEnabled = false;
+
+    } else {
+
+      this.backgroundColor = "black";
+      this.color = "white";
+      this.darkText = "Activar modo claro";
+      this.isDarkModeEnabled = true;
+
+    }
+
   }
 
 }
